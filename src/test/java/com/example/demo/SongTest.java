55ac84df7ea74dc56342a519301ce626ef92afb7
Lisa Martinez <LisaMartinez@lisas-MacBook-Air.local>
2018-11-14 16:16:07 -0500

    Initial commit

---
+++ src/test/java/com/example/demo/SongTest.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import org.junit.Test;
+
+public class SongTest {
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void getAlbum() {
+    }
+
+    @Test
+    public void setAlbum() {
+    }
+
+    @Test
+    public void getRating() {
+    }
+
+    @Test
+    public void setRating() {
+    }
+
+    @Test
+    public void getYear() {
+    }
+
+    @Test
+    public void setYear() {
+    }
+}
